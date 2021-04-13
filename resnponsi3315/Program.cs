0fbc07adbfb26c3dc4e03dabcea8bc3d1df91f9e
pacar nya Im Nayeon <izunaka8@gmail.com>
2021-04-13 12:24:37 +0700

    Add project files.

---
+++ resnponsi3315/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ResponsiPemrograman3315
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kar1 = new Karyawan(304111233, "Paijo", 3500000);
+            Karyawan kar2 = new Karyawan(304111234, "Jono", 2000000);
+
+            Karyawan.Show(kar1, kar2);
+            Karyawan.NaikGaji(kar1, kar2);
+            Karyawan.Show(kar1, kar2);
+        }
+    }
+}
